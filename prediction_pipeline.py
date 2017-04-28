9f73c775d95049fab2e93486ed61a33c6c057bd4
Satchel Grant <grantsrb@gmail.com>
2017-04-28 10:28:48 -0700

    fix error in numpy import

--- prediction_pipeline.py
+++ prediction_pipeline.py
@@ -1,4 +1,5 @@
 import time
+import numpy as np
 
 import inout
 import prediction as pred
@@ -7,18 +8,21 @@ import model as mod
 from multiprocessing.pool import ThreadPool
 
 ############### User Defined Variables
-data_path = './data/test'
+data_path = './resized'
 model_path = 'model.h5'
-test_divisions = 30 # Used for segmenting image evaluation in threading
-batch_size = 128 # Batch size used for keras predict function
+resize_dims = (256,256,3)
+test_divisions = 10 # Used for segmenting image evaluation in threading
+batch_size = 100 # Batch size used for keras predict function
 
 ############## Create Model
+from keras.models import Sequential, Model
+
 ins, outs = mod.cnn_model()
 model = Model(inputs=ins,outputs=outs)
 model.load_weights(model_path)
 
 ############# Read in Data
-test_paths, test_labels = read_paths(data_path,no_labels=True)
+test_paths, test_labels, _ = inout.read_paths(data_path,no_labels=True)
 print(str(len(test_paths))+' testing images')
 
 
@@ -27,23 +31,26 @@ predictions = []
 pool = ThreadPool(processes=1)
 portion = len(test_paths)//test_divisions+1 # Number of images to read in per pool
 
-async_result = pool.apply_async(convert_images,(test_paths[0*portion:portion*(0+1)],
+async_result = pool.apply_async(inout.convert_images,(test_paths[0*portion:portion*(0+1)],
                                                 test_labels[0*portion:portion*(0+1)],resize_dims))
 
 
 total_base_time = time.time()
-
+test_imgs = []
 for i in range(1,test_divisions+1):
     base_time = time.time()
 
     print("Begin set " + str(i))
-    test_imgs,_ = async_result.get()
+    while len(test_imgs) == 0:
+        test_imgs,_ = async_result.get()
+    img_holder = test_imgs
+    test_imgs = []
 
     if i < test_divisions:
-        async_result = pool.apply_async(convert_images,(test_paths[i*portion:portion*(i+1)],
+        async_result = pool.apply_async(inout.convert_images,(test_paths[i*portion:portion*(i+1)],
                                                         test_labels[0*portion:portion*(0+1)],resize_dims))
 
-    predictions.append(model.predict(test_imgs,batch_size=batch_size,verbose=0))
+    predictions.append(model.predict(img_holder,batch_size=batch_size,verbose=0))
     print("Execution Time: " + str((time.time()-base_time)/60)+'min\n')
 
 predictions = np.concatenate(predictions, axis=0)
