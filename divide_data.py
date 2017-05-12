3b32a7fb3ceb0ca1a9eab097e809b5687dd1113f
Satchel Grant <grantsrb@gmail.com>
2017-05-12 10:51:18 -0700

    add user friendly variables at top of file

--- divide_data.py
+++ divide_data.py
@@ -1,14 +1,17 @@
 from utilities import inout
 from sklearn.utils import shuffle
 
+train_csv = 'train_set.csv'
+valid_csv = 'valid_set.csv'
+
 root_path = 'resized/train'
 image_paths, labels, n_labels = inout.read_paths(root_path)
 
-root_paths = ['resized/Type_1', 'resized/Type_2', 'resized/Type_3']
-for i,root_path in enumerate(root_paths):
-    new_paths, new_labels, _ = inout.read_paths(root_path,label_type=i)
-    image_paths += new_paths
-    labels += new_labels
+# root_paths = ['resized/Type_1', 'resized/Type_2', 'resized/Type_3']
+# for i,root_path in enumerate(root_paths):
+#     new_paths, new_labels, _ = inout.read_paths(root_path,label_type=i)
+#     image_paths += new_paths
+#     labels += new_labels
 
 image_paths, labels = shuffle(image_paths, labels)
 
@@ -22,5 +25,5 @@ print(len(X_train_paths))
 print("Valid size: ")
 print(len(X_valid_paths))
 
-inout.save_paths('train_set.csv', X_train_paths, y_train)
-inout.save_paths('valid_set.csv', X_valid_paths, y_valid)
+inout.save_paths(train_csv, X_train_paths, y_train)
+inout.save_paths(valid_csv, X_valid_paths, y_valid)
