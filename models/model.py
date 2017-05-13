9991e8dc5e932cc6b3f55d98a62d7b5ef55bdae2
Satchel Grant <grantsrb@gmail.com>
2017-05-13 16:29:33 -0700

    add new model that uses 1x1 filtering prior to convolutions

--- models/model.py
+++ models/model.py
@@ -12,7 +12,7 @@ def cnn_model(first_conv_shapes=[(4,4),(3,3),(5,5)], conv_shapes=[(3,3),(5,5)], 
     zen_layer = BatchNormalization()(inputs)
 
     for shape in first_conv_shapes:
-        stacks.append(Conv2D(conv_depths[0], shape, padding='same', activation='elu')(inputs))
+        stacks.append(Conv2D(conv_depths[0], shape, padding='same', activation='elu')(zen_layer))
     layer = concatenate(stacks,axis=-1)
     layer = BatchNormalization()(layer)
     layer = MaxPooling2D(pooling_filter,strides=pooling_stride,padding='same')(layer)
@@ -34,6 +34,44 @@ def cnn_model(first_conv_shapes=[(4,4),(3,3),(5,5)], conv_shapes=[(3,3),(5,5)], 
         fclayer = Dense(dense_shapes[i], activation='elu')(fclayer)
     #     if i == 0:
     #         fclayer = Dropout(0.5)(fclayer)
+        fclayer = BatchNormalization()(fclayer)
+
+    outs = Dense(dense_shapes[-1], activation='softmax')(fclayer)
+
+    return inputs, outs
+
+
+def cnn_model_1x1(conv_shapes=[(3,3),(5,5)], conv_depths=[8,10,13,15,20], dense_shapes=[100,50,3], image_shape=(256,256,3), n_labels=3,ones_depth=15):
+    pooling_filter = (2,2)
+    pooling_stride = (2,2)
+
+    inputs = Input(shape=image_shape)
+    layer = BatchNormalization()(inputs)
+
+    stacks = []
+    for shape in conv_shapes:
+        stacks.append(Conv2D(conv_depths[0],shape,padding='same',activation='elu')(layer))
+    layer = concatenate(stacks,axis=-1)
+    layer = MaxPooling2D(pooling_filter,strides=pooling_stride, padding='same')(layer)
+    layer = BatchNormalization()(layer)
+
+    for i in range(1,len(conv_depths)):
+        stacks = []
+        stacks.append(Conv2D(ones_depth,[1,1],padding='same',activation='elu')(layer))
+        for shape in conv_shapes:
+            stacks.append(Conv2D(conv_depths[i],shape,padding='same',activation='elu')(stacks[0]))
+        layer = concatenate(stacks,axis=-1)
+        layer = MaxPooling2D(pooling_filter,strides=pooling_stride, padding='same')(layer)
+        layer = BatchNormalization()(layer)
+        layer = Dropout(.05)(layer)
+
+    layer = Conv2D(ones_depth,[1,1],padding='same',activation='elu')(layer)
+
+    fclayer = Flatten()(layer)
+    fclayer = Dropout(0.2)(fclayer)
+
+    for i in range(len(dense_shapes)-1):
+        fclayer = Dense(dense_shapes[i], activation='elu')(fclayer)
         fclayer = BatchNormalization()(fclayer)
 
     outs = Dense(dense_shapes[-1], activation='softmax')(fclayer)
