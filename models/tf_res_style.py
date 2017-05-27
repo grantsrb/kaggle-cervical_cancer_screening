bd3fb625e8236570eba037b4302b9fca9cb21bb8
Satchel Grant <grantsrb@gmail.com>
2017-05-27 16:06:46 -0700

    add layer dict, remove max pooling

--- models/tf_res_style.py
+++ models/tf_res_style.py
@@ -63,7 +63,8 @@ def get_tensors(img_shape, n_classes=3, res_depths=[20,20,20], n_resblocks=4, id
     penult_bias = tf.Variable(tf.zeros(n_classes), name='penult_b')
     tensordict['penult'] = (penult_weight, penult_bias)
 
-    tensordict['glob_avg_pool'] = img_shape[0]//(2**n_resblocks)
+    # tensordict['glob_avg_pool'] = img_shape[0]//(2**n_resblocks)
+    tensordict['glob_avg_pool'] = img_shape[0]
 
     dense_w = tf.Variable(tf.truncated_normal([n_classes, n_classes]))
     dense_b = tf.Variable(tf.zeros([n_classes]))
@@ -73,26 +74,30 @@ def get_tensors(img_shape, n_classes=3, res_depths=[20,20,20], n_resblocks=4, id
 
 
 def create(inputs, tensordict):
-
+    layers = dict()
     normed = batch_norm(inputs, name='normed')
     init_weight, init_bias = tensordict['init']
     res_layer = conv2d(normed, init_weight, init_bias, 'init_conv')
+    layers['init'] = res_layer
 
     n_resblocks = tensordict['n_resblocks']
     for i in range(n_resblocks):
         ws, bs, eye = tensordict['resblock'+str(i)]
         res_layer = res_block(res_layer, ws, bs, eye, 'res'+str(i))
-        res_layer = max_pooling(res_layer, 'res'+str(i))
+        layers['res_block'+str(i)] = res_layer
 
     penult_weight, penult_bias = tensordict['penult']
     penult_layer = conv2d(res_layer, penult_weight, penult_bias, 'penult')
+    layers['penult'] = penult_layer
 
     k = tensordict['glob_avg_pool']
     penult_layer = global_avg_pooling(penult_layer,k,'penult')
     vector_len = tensordict['n_classes']
-    penult_layer = tf.reshape(penult_layer, [-1, vector_len])
+    outs = tf.reshape(penult_layer, [-1, vector_len])
+    outs = outs / 10**8
+    layers['glob_avg_pool'] = outs
 
     # dense_w, dense_b = tensordict['dense']
     # final_layer = dense(penult_layer, dense_w, dense_b, name='final')
 
-    return penult_layer
+    return outs, layers
